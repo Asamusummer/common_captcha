2da4135a1951235ebd8d8f4399ef3ef79d7dd498
王磊 <wanglei989@guance.com>
2023-12-20 10:47:35 +0800

    补充修改对应的setup

---
+++ README.rst
@@ -0,0 +1,30 @@
+=========================
+common_captcha
+=========================
+
+common captcha for web server
+
+Introdution
+-----------
+
+通用验证码，包括简单验证码，数字，字母，滑块验证码等
+
+
+Requirements
+~~~~~~~~~~~~
+
+- `python`_ 3.6+
+- `redis`_ latest
+
+
+Use Guide
+---------
+
+Installtion
+~~~~~~~~~~~
+
+Install with pip:
+
+.. code-block:: console
+
+    $ python -m pip install common_captcha
--- setup.py
+++ setup.py
@@ -1,28 +1,8 @@
-import setuptools
+#!/usr/bin/env python
+from __future__ import annotations
 
+__author__ = "lei.wang"
 
-with open('README.md', 'r') as fp:
-    long_description = fp.read()
+from setuptools import setup
 
-
-setuptools.setup(
-    name="common-captcha",
-    version="0.0.1",
-    author="lei.wang",
-    author_email="1061379682@qq.com",
-    description="package for common common_captcha",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        'redis',
-        'pillow',
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+setup()
